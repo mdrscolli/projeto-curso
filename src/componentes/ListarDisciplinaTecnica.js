1fd3980ef606b3f243ab2afec05901b4c96e7d10
mdrscolli <mariana.colli.santos@escola.pr.gov.br>
2026-05-26 12:30:14 -0300

    Add files via upload

--- src/componentes/ListarDisciplinaTecnica.js
+++ src/componentes/ListarDisciplinaTecnica.js
@@ -1,7 +1,30 @@
+import dadosDisciplinasTecnicas from'../dados/dadosDisciplinasTecnicas'
+
 function ListarDisciplinaTecnica() {
 
     return (
-        <H1>Listar Disciplina Tecnica</H1>
+        <section className='disciplinas'>
+            <header className='headerDisciplinas'>
+                <h1>Disciplinas Técnicas</h1>
+            </header>
+                <div  className='contentDisciplinas'>
+            
+            
+            {
+                dadosDisciplinasTecnicas.map(
+                    (item, index)=>(
+                        <artcle key={index}>
+                        <img src={item.link_imagem}
+                        alt='{item, nome}'
+                        title='{item.nome}' />
+                        {item.nome} - {item.sigla}
+                        </artcle>
+                    )
+                )
+            }
+            </div>          
+ </section>
+        
     )
 
 }
---
+++ src/dados/dadosDisciplinasTecnicas.js
@@ -0,0 +1,33 @@
+const dadosDisciplinasTecnicas = [
+    {
+        id:1,
+        nome: 'Lógica Computacional',
+        sigla:'LC',
+        ano:2025,
+        serie:'1°A',
+        descricao:'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+        link_imagem:'https://placehold.co/200x100',
+    },
+
+    {
+        id:2,
+        nome: 'Introdução à Computação',
+        sigla:'IC',
+        ano:2025,
+        serie:'1°A',
+        descricao:'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+        link_imagem:'https://placehold.co/200x100',
+    },
+
+
+
+    {
+        id:3,
+        nome: 'Introdução à Programação',
+        sigla:'IP',
+        ano:2025,
+        serie:'1°A',
+        descricao:'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+        link_imagem:'https://placehold.co/200x100',
+    },
+]
